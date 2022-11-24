8fa46c79dd4a684c71786f93370fdb9c0a7f3826
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2022-11-24 09:23:52 +0800

    fix missing image bug by adding placeholder image

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -29,6 +29,13 @@ const ItemPreview = (props) => {
     }
   };
 
+  // check if item.image is null
+  const itemImage = item.image ? (
+    item.image
+  ) : (
+    "placeholder.png"
+  );
+
   return (
     <div
       className="card bg-dark border-light p-3"
@@ -36,7 +43,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={itemImage}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
